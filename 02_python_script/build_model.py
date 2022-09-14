199d34a7d548554c799a503a534be639166e72da
Taishi Miyata <taishimiyata@microsoft.com>
2022-09-14 11:30:11 +0000

    init

--- 02_python_script/build_model.py
+++ 02_python_script/build_model.py
@@ -171,139 +171,27 @@ class ImageTransform:
 
 
 def load_data(train_data_path: str, val_data_path: str):
-    # データパスの処理
-    train_list = make_datapath_list(train_data_path)
-    val_list = make_datapath_list(val_data_path)
-
-    # 画像変換+データセット作成
-    size = 224
-    mean = (0.485, 0.456, 0.406)
-    std = (0.229, 0.224, 0.225)
-    train_dataset = HymenopteraDataset(
-        file_list=train_list,
-        transform=ImageTransform(size, mean, std),
-        phase="train",
-    )
-    val_dataset = HymenopteraDataset(
-        file_list=val_list,
-        transform=ImageTransform(size, mean, std),
-        phase="val",
-    )
-
-    # 動作確認
-    index = 0
-    print(train_dataset.__getitem__(index)[0].size())
-    print(train_dataset.__getitem__(index)[1])
-    # ミニバッチのサイズを指定
-    batch_size = 32
-
-    # DataLoaderを作成
-    train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True
-    )
-
-    val_dataloader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=False
-    )
-
-    # 辞書型変数にまとめる
-    dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
-
-    return dataloaders_dict
+    ## 記述 ##
+    return ""
 
 
 def create_network():
-    net = models.vgg16(pretrained=True)
-    net.classifier[6] = nn.Linear(in_features=4096, out_features=2)
-    net.train()
-    return net
+    ## 記述 ##
+    return ""
 
 
 def set_optimizer():
-    params_to_update = []
-    update_param_names = ["classifier.6.weight", "classifier.6.bias"]
-    for name, param in net.named_parameters():
-        if name in update_param_names:
-            param.requires_grad = True
-            params_to_update.append(param)
-            print(name)
-        else:
-            param.requires_grad = False
-    optimizer = optim.SGD(params=params_to_update, lr=0.001, momentum=0.9)
-    return optimizer
+    ## 記述 ##
+    return ""
 
 
 def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs):
-
-    # epochのループ
-    for epoch in range(num_epochs):
-        print("Epoch {}/{}".format(epoch + 1, num_epochs))
-        print("-------------")
-
-        # epochごとの学習と検証のループ
-        for phase in ["train", "val"]:
-            if phase == "train":
-                net.train()  # モデルを訓練モードに
-            else:
-                net.eval()  # モデルを検証モードに
-
-            epoch_loss = 0.0  # epochの損失和
-            epoch_corrects = 0  # epochの正解数
-
-            # 未学習時の検証性能を確かめるため、epoch=0の訓練は省略
-            if (epoch == 0) and (phase == "train"):
-                continue
-
-            # データローダーからミニバッチを取り出すループ
-            for inputs, labels in dataloaders_dict[phase]:
-
-                # optimizerを初期化
-                optimizer.zero_grad()
-
-                # 順伝搬（forward）計算
-                with torch.set_grad_enabled(phase == "train"):
-                    outputs = net(inputs)
-                    loss = criterion(outputs, torch.tensor(labels))  # 損失を計算
-                    _, preds = torch.max(outputs, 1)  # ラベルを予測
-
-                    # 訓練時はバックプロパゲーション
-                    if phase == "train":
-                        loss.backward()
-                        optimizer.step()
-
-                    # イタレーション結果の計算
-                    # lossの合計を更新
-                    epoch_loss += loss.item() * inputs.size(0)
-                    # 正解数の合計を更新
-                    epoch_corrects += torch.sum(preds == labels.data)
-
-            # epochごとのlossと正解率を表示
-            epoch_loss = epoch_loss / len(dataloaders_dict[phase].dataset)
-            epoch_acc = epoch_corrects.double() / len(
-                dataloaders_dict[phase].dataset
-            )
-
-            print(
-                "{} Loss: {:.4f} Acc: {:.4f}".format(
-                    phase, epoch_loss, epoch_acc
-                )
-            )
-
-            mlflow.log_metric(phase + "_loss", epoch_loss, step=epoch)
-            mlflow.log_metric(phase + "_acc", epoch_acc, step=epoch)
-    return net
+    ## 記述 ##
+    return ""
 
 
 def register_model(net, run):
-    model_name = "model.pth"
-    local_path = f"./{model_name}"
-    torch.save(net.state_dict(), local_path)
-    mlflow.log_artifact(local_path, "output/")
-    run_id = run.info.run_id
-    mlflow.register_model(
-        model_uri=f"runs:/{run_id}/output/{model_name}",
-        name="transfer_learning_model",
-    )
+    ## 記述 ##
     return ""
 
 
